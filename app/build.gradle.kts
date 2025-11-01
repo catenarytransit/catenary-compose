9622ea86d7028a9db0dbcec4ff2460584b98a70c
Kyler Chin <7539174+kylerchin@users.noreply.github.com>
2025-11-01 11:24:44 -0700

    Bump version numbers

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.catenarymaps.catenary"
         minSdk = 27
         targetSdk = 36
-        versionCode = 107
-        versionName = "2.0.5alpha"
+        versionCode = 108
+        versionName = "2.0.6alpha"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
