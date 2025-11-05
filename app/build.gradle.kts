199c0b522718b2db902bd32851d7426757cca60c
Kyler Chin <7539174+kylerchin@users.noreply.github.com>
2025-11-04 20:02:24 -0800

    Increase version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,8 +24,8 @@ android {
         applicationId = "com.catenarymaps.catenary"
         minSdk = 27
         targetSdk = 36
-        versionCode = 112
-        versionName = "2.0.10alpha"
+        versionCode = 113
+        versionName = "2.0.11alpha"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
