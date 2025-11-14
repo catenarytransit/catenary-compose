1da34c6320d5144289f4deefe9f9c02716f39ab3
Kyler Chin <7539174+kylerchin@users.noreply.github.com>
2025-11-13 20:49:49 -0800

    Print the error

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,8 +32,8 @@ android {
         applicationId = "com.catenarymaps.catenary"
         minSdk = 27
         targetSdk = 36
-        versionCode = 153
-        versionName = "2.0.51"
+        versionCode = 154
+        versionName = "2.0.52"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
