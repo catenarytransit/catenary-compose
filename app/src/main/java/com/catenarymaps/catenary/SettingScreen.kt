562b213145679ec775ff443cd1148c5c789af3bc
Kyler Chin <7539174+kylerchin@users.noreply.github.com>
2025-11-14 20:37:07 -0800

    correct the padding on settings

--- app/src/main/java/com/catenarymaps/catenary/SettingScreen.kt
+++ app/src/main/java/com/catenarymaps/catenary/SettingScreen.kt
@@ -5,9 +5,12 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.safeDrawing
+import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
@@ -18,6 +21,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 
@@ -36,6 +40,13 @@ fun SettingsScreen(
             .fillMaxWidth()
             .verticalScroll(rememberScrollState())
             .padding(vertical = 8.dp)
+            .windowInsetsPadding(
+                WindowInsets(
+                    bottom = WindowInsets.safeDrawing.getBottom(
+                        density = LocalDensity.current
+                    )
+                )
+            )
     ) {
         Row(
             modifier = Modifier.fillMaxWidth(),
