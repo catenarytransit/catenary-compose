4b38803a5ab32cbdfb977607c67a70e6583553a6
Kyler Chin <7539174+kylerchin@users.noreply.github.com>
2025-11-14 22:38:15 -0800

    release version v2.0.60

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,8 +32,8 @@ android {
         applicationId = "com.catenarymaps.catenary"
         minSdk = 27
         targetSdk = 36
-        versionCode = 161
-        versionName = "2.0.59"
+        versionCode = 162
+        versionName = "2.0.60"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
