8cd7cc7ac415c75bc0396095d3220442dad723e1
Kyler Chin <7539174+kylerchin@users.noreply.github.com>
2025-12-31 08:09:52 -0800

    fix missing imports

--- app/src/main/java/com/catenarymaps/catenary/StopScreenRows.kt
+++ app/src/main/java/com/catenarymaps/catenary/StopScreenRows.kt
@@ -13,7 +13,8 @@ import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
-import com.catenarymaps.catenary.StopEvent // redundant but safe?
+import androidx.compose.runtime.Composable
+import com.catenarymaps.catenary.StopEvent
 import androidx.compose.foundation.layout.width
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
