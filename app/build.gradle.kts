9d6ef40115341fde9d2ba7ece6a7a62fa05af740
Kyler Chin <7539174+kylerchin@users.noreply.github.com>
2026-06-12 13:23:49 -0700

    bump version up to 162

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,8 +25,8 @@ android {
         applicationId = "com.catenarymaps.catenary"
         minSdk = 27
         targetSdk = 36
-        versionCode = 263
-        versionName = "2.0.161"
+        versionCode = 264
+        versionName = "2.0.162"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
